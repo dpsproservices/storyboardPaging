6a9f27d9b8d0902e9ad76121ebcdebb3b203dd24
Joseph Skarulis <jskarulis@gmail.com>
2015-04-29 17:18:59 -0400

    storyboard UIPageViewController example, no segues

---
+++ StoryboardPagingDemo/ContentViewControllers/BaseContentViewController.m
@@ -0,0 +1,27 @@
+//
+//  BaseContentViewController.m
+//  StoryboardPagingDemo
+//
+//  Created by Derek Lee on 2/6/15.
+//  Copyright (c) 2015 Derek Lee. All rights reserved.
+//
+
+#import "BaseContentViewController.h"
+
+@interface BaseContentViewController ()
+
+@end
+
+@implementation BaseContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
